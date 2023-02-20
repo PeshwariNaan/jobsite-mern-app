93247c69277e08ad86c705919be123f645e9285f
J.D. Browne <PeshwariNaan01@proton.me>
2023-02-20 15:54:09 +0100

    Added index files for pages and components for organization.

---
+++ client/src/pages/index.jsx
@@ -0,0 +1,6 @@
+import LandingPage from './LandingPage';
+import ErrorPage from './ErrorPage';
+import DashboardPage from './DashboardPage';
+import RegisterPage from './RegisterPage';
+
+export { LandingPage, ErrorPage, DashboardPage, RegisterPage };
