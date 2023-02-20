c9ae45792f2c6d39c8901114870e65c2b7cf0e6e
J.D. Browne <PeshwariNaan01@proton.me>
2023-02-20 15:32:17 +0100

    Added react router and set up initial routes.

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,17 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { LandingPage, RegisterPage, DashboardPage, ErrorPage } from './pages';
+
 function App() {
-  return <h1> JobHunter </h1>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<DashboardPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/landing" element={<LandingPage />} />
+        <Route path="*" element={<ErrorPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
