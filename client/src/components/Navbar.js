057665c77dad9903956f518b888795b0e46010b2
J.D. Browne <PeshwariNaan01@proton.me>
2023-02-27 10:44:05 +0100

    Added basic layout for Navbar component.

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,11 +1,45 @@
+import { useState } from 'react';
+import { useAppContext } from '../context/appContext';
 import Wrapper from '../assets/wrappers/Navbar';
-import { FaHome } from 'react-icons/fa';
+import Logo from './Logo';
+import { FaAlignLeft, FaUserCircle, FaCaretDown } from 'react-icons/fa';
 
 const Navbar = () => {
   return (
     <Wrapper>
-      <h1>Navbar</h1>
-      <FaHome />
+      <div className="nav-center">
+        <button
+          type="button"
+          className="toggle-btn"
+          onClick={() => console.log('toggle sidebar')}
+        >
+          <FaAlignLeft />
+        </button>
+        <div>
+          <Logo />
+          <h3 className="logo-text">Dashboard</h3>
+        </div>
+        <div className="btn-container">
+          <button
+            type="button"
+            className="btn"
+            onClick={() => console.log('cshow-hide dropdown')}
+          >
+            <FaUserCircle />
+            Joshua
+            <FaCaretDown />
+          </button>
+          <div className="dropdown show-dropdown">
+            <button
+              type="button"
+              className="dropdown-btn"
+              onClick={() => console.log('logout user')}
+            >
+              Logout
+            </button>
+          </div>
+        </div>
+      </div>
     </Wrapper>
   );
 };
