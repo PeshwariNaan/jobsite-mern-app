07f93cd37d09cb82ec8cbaafe9d17e6b02fb002a
J.D. Browne <PeshwariNaan01@proton.me>
2023-03-07 13:26:50 +0100

    Added AreaChartComponent and BarChartComponent to display graph in ChartsContainer component.

--- client/src/components/ChartsContainer.js
+++ client/src/components/ChartsContainer.js
@@ -1,8 +1,24 @@
+import { useState } from 'react';
+import { useAppContext } from '../context/appContext';
+import AreaChartComponent from './AreaChartComponent';
+import BarChartComponent from './BarChartComponent';
+import Wrapper from '../assets/wrappers/ChartsContainer';
+
 const ChartsContainer = () => {
+  const { monthlyApplications: data } = useAppContext();
+  const [barChart, setBarChart] = useState(true);
   return (
-    <div>
-      <h1>Charts Container</h1>
-    </div>
+    <Wrapper>
+      <h4>Monthly Applications</h4>
+      <button type="button" onClick={() => setBarChart(!barChart)}>
+        {barChart ? 'Area Chart' : 'Bar Chart'}
+      </button>
+      {barChart ? (
+        <BarChartComponent data={data} />
+      ) : (
+        <AreaChartComponent data={data} />
+      )}
+    </Wrapper>
   );
 };
 export default ChartsContainer;
