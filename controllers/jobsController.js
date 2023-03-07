38d1bfca332c8f2c308d64b0988baf3b21fc46a4
J.D. Browne <PeshwariNaan01@proton.me>
2023-03-07 12:07:12 +0100

    Added aggregation pipeline for monthly applications in jobsController.

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -96,7 +96,24 @@ export const showStats = async (req, res) => {
     interview: stats.interview || 0,
     declined: stats.declined || 0,
   };
-  let monthlyApplications = [];
+  let monthlyApplications = await Job.aggregate([
+    { $match: { createdBy: mongoose.Types.ObjectId(req.user.userId) } },
+    {
+      $group: {
+        _id: {
+          year: {
+            $year: '$createdAt',
+          },
+          month: {
+            $month: '$createdAt',
+          },
+        },
+        count: { $sum: 1 },
+      },
+    },
+    { $sort: { '_id.year': -1, '_id.month': -1 } },
+    { $limit: 6 },
+  ]);
 
   res.status(StatusCodes.OK).json({ defaultStats, monthlyApplications });
 };
