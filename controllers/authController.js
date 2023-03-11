60de56b67120542ad8e8d4a47c1b5552fc7d94a4
J.D. Browne <PeshwariNaan01@proton.me>
2023-03-11 09:15:00 +0100

    Added attacheCookie.js. Added function to auth controllers.

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,7 +1,9 @@
 import { StatusCodes } from 'http-status-codes';
 import User from '../models/userModel.js';
 import { BadRequestError, UnathenticatedError } from '../errors/index.js';
+import attachCookie from '../utils/attachCookie.js';
 
+//REGISTER CONTROLLER
 const register = async (req, res) => {
   // We are not using try catch blocks but rather the npm express-async-errors package - No need to use next
   const { name, email, password } = req.body;
@@ -14,6 +16,7 @@ const register = async (req, res) => {
   }
   const user = await User.create({ name, email, password });
   const token = user.createJWT();
+  attachCookie({ res, token });
   res.status(StatusCodes.CREATED).json({
     //Here we hardcoded the values that we want on the response. We did this to avoid getting the PW back because it will be sent with .create method
     user: {
@@ -26,6 +29,7 @@ const register = async (req, res) => {
   });
 };
 
+//LOGIN CONTROLLER
 const login = async (req, res) => {
   const { email, password } = req.body;
   if (!email || !password) {
@@ -43,6 +47,7 @@ const login = async (req, res) => {
   }
   const token = user.createJWT();
   user.password = undefined;
+  attachCookie({ res, token });
 
   res.status(StatusCodes.OK).json({ user, token, location: user.location });
 };
@@ -62,6 +67,7 @@ const updateUser = async (req, res) => {
 
   await user.save(); // user.save() can cause issues
   const token = user.createJWT();
+  attachCookie({ res, token });
 
   res.status(StatusCodes.OK).json({ user, token, location: user.location });
 };
