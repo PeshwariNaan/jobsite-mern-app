3449e1d147409a733e0757e8f8ecc25591788676
J.D. Browne <PeshwariNaan01@proton.me>
2023-06-14 10:32:20 +0200

    Fixing weird error on logo.

--- client/src/components/Logo.jsx
+++ client/src/components/Logo.jsx
@@ -1,6 +1,6 @@
-import logo from '../assets/images/logo.svg';
+//import logo from '../assets/images/logo.svg';
 import logoTwo from '../assets/images/logo-two.svg';
-import logoThree from '../assets/images/logo-three.svg';
+//import logoThree from '../assets/images/logo-three.svg';
 
 const Logo = () => {
   return <img src={logoTwo} alt="Job-hunter" className="logo" />;
